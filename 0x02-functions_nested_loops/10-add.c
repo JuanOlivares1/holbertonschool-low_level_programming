87c4b20f9038ceb87e35e4866949c0389e20867a
Juan Olivares <jdop2000@gmail.com>
2020-02-11 15:47:22 +0000

    #10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - Adds the two arguments parameters.
+ * @a, @b: the two integers to add.
+ *
+ * Return: The result of operation.
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
