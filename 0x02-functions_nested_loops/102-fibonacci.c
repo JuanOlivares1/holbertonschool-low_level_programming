4715e02a232ba95166e67c56cd703f1bfc5da889
Juan Olivares <jdop2000@gmail.com>
2020-02-11 21:54:15 +0000

    #102

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,25 +7,26 @@
  */
 int main(void)
 {
-	int num1, num2, tmp, count;
+	unsigned long num1, num2, tmp;
+	int count;
 
 	num1 = 1;
 	num2 = 2;
 	count = 2;
 
-	printf("%i", num1);
+	printf("%lu", num1);
 	printf(", ");
-	printf("%i", num2);
+	printf("%lu", num2);
 
-	while (count <= 50)
+	while (count < 50)
 	{
-		if (count <= 50)
+		if (count < 50)
 			printf(", ");
 		tmp = num1;
 		num1 = num2;
 		num2 = tmp + num1;
 		count++;
-		printf("%i", num2);
+		printf("%lu", num2);
 	}
 	printf("\n");
 	return (0);
