c4e627b8992fea0a0e1d7345c0172e2d7cac004a
Juan Olivares <jdop2000@gmail.com>
2020-02-13 19:59:04 +0000

    retry

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_triangle - function that prints a triangle, followed by a new line.
+ * @size: determines the size of the triangle.
+ *
+ * Return: Void.
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j <= i)
+				_putchar('#');
+			else
+				_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz-buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz-buzz.c
@@ -23,7 +23,8 @@ int main(void)
 		{
 			printf("%d", i);
 		}
-		putchar(' ');
+		if (i != 100)
+			putchar(' ');
 	}
 	putchar('\n');
 	return (0);
