209ac9d24a5a789f464c66c89d9211500ad03ff6
Juan Olivares <jdop2000@gmail.com>
2020-02-13 23:34:09 +0000

    #101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,35 +7,19 @@
  */
 void print_number(int n)
 {
+	unsigned int i;
+
 	if (n < 0)
 	{
-		n *= -1;
-	}
-
-	if (n / 10000 != 0)
-	{
-		_putchar((n / 10000) + '0');
-		_putchar(((n / 1000) % 10) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	} else if (n / 1000 != 0)
-	{
-		_putchar((n / 1000) + '0');
-		_putchar(((n / 100) % 10) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	} else if (n / 100 != 0)
-	{
-		_putchar((n / 100) + '0');
-		_putchar(((n / 10) % 10) + '0');
-		_putchar((n % 10) + '0');
-	} else if (n / 10 != 0)
-	{
-		_putchar((n / 10) + '0');
-		_putchar((n % 10) + '0');
+		i = -n;
+		_putchar('-');
 	} else
 	{
-		_putchar(n + '0');
+		i = n;
 	}
+
+	if (i / 10)
+		print_number(i / 10);
+
+	_putchar((i % 10) + '0');
 }
