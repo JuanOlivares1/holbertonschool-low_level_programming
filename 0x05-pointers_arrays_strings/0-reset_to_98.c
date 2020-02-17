d50e494e8c83c37c2e0f5c1cfbbca180337bb655
Juan Olivares <jdop2000@gmail.com>
2020-02-17 20:45:24 +0000

    #0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * reset_to_98 - updates value the pointer pints to.
+ * @n: the pointer where the value will be reset.
+ *
+ * Return: void.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
