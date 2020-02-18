4f39852e839ef394a0c600e19a421e4cbd7f9509
Juan Olivares <jdop2000@gmail.com>
2020-02-18 18:49:29 +0000

    #6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * puts2 - prints every other character of a string.
+ * @str: string to print
+ *
+ * Returns: void.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if(i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
