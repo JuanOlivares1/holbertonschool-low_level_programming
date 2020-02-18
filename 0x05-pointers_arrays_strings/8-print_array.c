27ace128038db06df23c20da57adf60f83881954
Juan Olivares <jdop2000@gmail.com>
2020-02-18 20:05:46 +0000

    #8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_array - prints number of elements given of an array.
+ * @a: string to print
+ * @n: number of elements to print
+ *
+ * Returns: void.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+			printf(", ");
+	}
+	putchar('\n');
+}
