929a04e872923e9cfb90e27c8d2a1a083f0a3db5
Juan Olivares <jdop2000@gmail.com>
2020-02-19 16:10:17 +0000

    #5 better

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
  * rev_string - reverse a string.
  * @s: string to print
@@ -11,15 +10,16 @@ void rev_string(char *s)
 	int i, j;
 	char tmp;
 
-	for (i = 0; s[i] != '\0'; ++i);
+	for (i = 0; s[i] != '\0'; ++i)
+		;
 
 	j = i - 1;
 
 	for (i = 0; i <= j; i++)
 	{
 		tmp = s[i];
-                s[i] = s[j];
-                s[j] = tmp;
+		s[i] = s[j];
+		s[j] = tmp;
 		j--;
 	}
 }
