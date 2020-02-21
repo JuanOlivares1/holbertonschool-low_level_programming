8e42908bb88ea317c2108c5e26147c9a6d1b1e06
Juan Olivares <jdop2000@gmail.com>
2020-02-21 04:01:08 +0000

    Refactor #6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,14 +7,11 @@
  */
 char *cap_string(char *s)
 {
-	int i, j;
+	int j;
 
-	for (i = 0; s[i] != '\0'; ++i)
-		;
-
-	for (j = 0; j <= i; j++)
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		if ((s[j] <= 'a' || s[j] >= 'z')
+		if ((s[j] < 'a' || s[j] > 'z')
 		    && (s[j + 1] >= 'a' && s[j + 1] <= 'z'))
 		{
 			if (s[j] == ' ' || s[j] == '\t'
