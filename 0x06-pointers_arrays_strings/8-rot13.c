3513739daed150ad12e210fd959f838eebd78107
Juan Olivares <jdop2000@gmail.com>
2020-02-21 03:22:04 +0000

    #8

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes on rot13
+ * @s: string to encode
+ *
+ * Returns: string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char r13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char val[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for(j = 0; j < 52; j++)
+		{
+			if (s[i] == r13[j])
+			{
+				s[i] = val[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
