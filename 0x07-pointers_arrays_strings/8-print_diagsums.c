61017ce4a9370faefe3e5ffe4cb923eadfbdf9b9
Juan Olivares <jdop2000@gmail.com>
2020-02-24 23:30:39 +0000

    #8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,15 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, k = 0, diag1, diag2;
+	int i, j, k = 0, diag1 = 0, diag2 = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
 		{
 			if (i == j)
-                                diag1 += a[k];
+				diag1 += a[k];
+
 			if (i + j + 1 == size)
 				diag2 += a[k];
 			k++;
