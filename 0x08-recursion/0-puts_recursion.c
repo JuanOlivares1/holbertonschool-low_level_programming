9f252c4a34051533443126d4697628b31b7efa1d
Juan Olivares <jdop2000@gmail.com>
2020-02-26 17:59:24 +0000

    #0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,12 +7,13 @@
  */
 void _puts_recursion(char *s)
 {
-	_putchar(s[0]);
-	s++;
 	if (s[0] == '\0')
 	{
+		_putchar('\0');
 		_putchar('\n');
 		return;
 	}
+	_putchar(s[0]);
+	s++;
 	_puts_recursion(s);
 }
