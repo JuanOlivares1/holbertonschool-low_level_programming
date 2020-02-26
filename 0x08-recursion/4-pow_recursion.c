8c35710f529ac113afd43d167dc4e94ed0732fe9
Juan Olivares <jdop2000@gmail.com>
2020-02-26 18:24:26 +0000

    #4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _pow_recursion - returns the value ox @x to the power of @y
+ * @x: base number
+ * @y: power
+ *
+ * Return: power of a number
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 1)
+		return (x);
+	else if (y > 1)
+		return (x * _pow_recursion(x, y - 1));
+	else if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (0);
+}
