30854b996b5eb7d7080b659071cf2759ee059c26
Juan Olivares <jdop2000@gmail.com>
2020-02-28 21:09:13 +0000

    creating a progran that prints its name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * main -
+ *
+ * Return: always 0. Success
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
