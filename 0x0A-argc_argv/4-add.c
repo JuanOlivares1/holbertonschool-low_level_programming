d122d59c097e86939d650922ae74d49385b23aa3
Juan Olivares <jdop2000@gmail.com>
2020-03-01 21:51:49 +0000

    chaging logic on program

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,11 +6,11 @@
  * @argc: number of arguments
  * @argv: pointer to an array of pointers (strings of arguments)
  *
- * Return: 0 succes, 1 fail
+ * Return: 0 success, 1 fail
  */
 int main(int argc, char **argv)
 {
-	int i, av, x = 0;
+	int i, j, x = 0;
 
 	if (argc <= 1)
 	{
@@ -20,18 +20,15 @@ int main(int argc, char **argv)
 
 	for (i = 1; i < argc; i++)
 	{
-		av = atoi(argv[i]);
-
-		if ((*argv[i] < '0' || *argv[i] > '9') && av < 0)
+		for (j = 0; argv[i][j]; j++)
 		{
-			printf("Error\n");
-			return (1);
-		} else if (*argv[i] < '0' || *argv[i] > '9')
-		{
-			printf("Error\n");
-			return (1);
-		} else if (av >= 0)
-			x += av;
+			if (argv[i][j] <= '0' || argv[i][j] >= '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		x += atoi(argv[i]);
 	}
 
 	printf("%i\n", x);
