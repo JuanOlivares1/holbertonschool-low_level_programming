247cd9f92a93ea072a872f5c22737cd70bc59832
Juan Olivares <jdop2000@gmail.com>
2020-03-03 22:49:07 +0000

    writing a function that free allocated memory

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * free_grid - free a matrix allocated space on memory
+ * @grid: matrix
+ * @height: height of matrix
+ *
+ * Return: pointer to string.
+ */
+void free_grid(int **grid, int height);
+{
+	int i;
+
+	for (i = 0; i < hight; i++)
+		free(grid[i]);
+	free(grid);
+}
