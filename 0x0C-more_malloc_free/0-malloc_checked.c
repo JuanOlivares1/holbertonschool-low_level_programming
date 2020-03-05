ed8b200ba128c33fc5f16488bd5cf61589df95b3
Juan Olivares <jdop2000@gmail.com>
2020-03-05 18:20:07 +0000

    first commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * malloc_checked - asd
+ * @b: asd
+ *
+ * Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+	{
+		free(p);
+		exit(98);
+	}
+	return(p);
+}
