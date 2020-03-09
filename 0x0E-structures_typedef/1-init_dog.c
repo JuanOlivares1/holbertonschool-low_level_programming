74373689e0dcca08991b1aa0e9295e76551415a5
Juan Olivares <jdop2000@gmail.com>
2020-03-09 23:00:38 +0000

    refactor #1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+
 /**
  * init_dog - initializes a dog structure
  * @d: pointer to the structure to initialice.
@@ -10,6 +11,8 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == 0)
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
