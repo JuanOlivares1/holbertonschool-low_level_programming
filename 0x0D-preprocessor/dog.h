114877df0c0740d9e2e211e087c32b5bc21164be
Juan Olivares <jdop2000@gmail.com>
2020-03-09 22:22:36 +0000

    creating a heder with a structure 'dog'

---
+++ 0x0D-preprocessor/dog.h
@@ -0,0 +1,21 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+int _putchar(char c);
+
+#endif
+
+/**
+ * struct dog - It's a dog, what can I say
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner name
+ *
+ * Description: It's a dog, what can I say
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
