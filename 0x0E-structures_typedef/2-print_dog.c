e3e933de07e34a14a27aa8479a20ec549821bca0
Juan Olivares <jdop2000@gmail.com>
2020-03-09 23:19:39 +0000

    creating a function that prints a dog structure

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_dog - initializesprints a dog structure
  * @d: pointer to the structure to printinitialice.
@@ -10,10 +11,10 @@ void print_dog(struct dog *d)
 {
 	if (d == 0)
 		return;
-	d->name == '\0' ? (printf("Name: (nil)\n"))
+	d->name == NULL ? (printf("Name: (nil)\n"))
 		: (printf("Name: %s\n", d->name));
-	d->age == 0 ? (printf("Age: (nil)\n"))
+	d->age == NULL ? (printf("Age: (nil)\n"))
 		: (printf("Age: %f\n", d->age));
-	d->owner == '\0' ? (printf("Owner: (nil)\n"))
+	d->owner == NULL ? (printf("Owner: (nil)\n"))
 		: (printf("Owner: %s\n", d->owner));
 }
