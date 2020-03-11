d5aff56de2108600ba6a402bccc7ad83b8840484
Juan Olivares <jdop2000@gmail.com>
2020-03-11 20:42:40 +0000

    my first programm :)

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - main function program.
+ * @ac: number of args
+ * @av: pointer to array of args(strings)
+ *
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	int a, b, result;
+	int (*op)(int, int);
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(av[1]);
+	b = atoi(av[3]);
+
+	if (sizeof(av[2]) != 1)
+	{
+		printf("Error\n");
+		exit(0);
+	}
+
+	if ((av[2][0] == '/' || av[2][0] == '%') && (av[1] == 0 || av[3] == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	op = get_op_func(av[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	result = op(a, b);
+	printf("%d\n", result);
+	return (0);
+}
