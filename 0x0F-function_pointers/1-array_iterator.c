a4c193cf84b961ee3f9f1d67b48e490f87d80063
Juan Olivares <jdop2000@gmail.com>
2020-03-11 19:00:40 +0000

    refactoring functions

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,6 +12,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
+	if (action == NULL || size = 0 || array == NULL)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
