c043d21bec0a1f35fdd741506a04968e150a9fa8
Juan Olivares <jdop2000@gmail.com>
2020-03-16 00:55:14 +0000

    asd

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,14 +8,39 @@
  */
 void print_all(const char * const format, ...)
 {
-	va_list list;
+	va_list li;
 	char *str;
+	int i = 0;
 
-	if (format == NULL)
-		return;
+	va_start(li, format);
 
-	va_start(list, ?);
-
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(li, int));
+			break;
+		case 'i':
+			printf("%i", va_arg(li, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(li, double));
+			break;
+		case 's':
+			str = va_arg(li, char *);
+			if (str == NULL)
+				str = "(nil)";
+			printf("%s", str);
+			break;
+		}
+		if (format[i + 1] != '\0' && (format[i] == 'c' ||
+					  format[i] == 'i' ||
+					  format[i] == 'f' ||
+					  format[i] == 's'))
+			printf(", ");
+		i++;
+	}
 	printf("\n");
-	va_end(list);
+	va_end(li);
 }
