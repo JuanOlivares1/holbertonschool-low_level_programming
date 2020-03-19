f93f94bec77f0c6ff008f925a7de24847818a49e
Juan Olivares <jdop2000@gmail.com>
2020-03-19 20:03:44 +0000

    refactoring function

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
  * print_list - prints all elements of a linked list
@@ -9,12 +9,8 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t elements = 0;
-
 	if (h->next == NULL)
-	{
-		return (elements + 1);
-	}
+		return (1);
 
 	if (h->str == NULL)
 	{
@@ -22,5 +18,5 @@ size_t print_list(const list_t *h)
 		return (print_list(h->next) + 1);
 	}
 	printf("[%d] %s\n", h->len, h->str);
-        return (print_list(h->next) + 1);
+	return (print_list(h->next) + 1);
 }
