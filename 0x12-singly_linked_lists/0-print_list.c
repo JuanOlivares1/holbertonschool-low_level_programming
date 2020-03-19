20513d8ebe36c58daf32ac551043fdfa14200589
Juan Olivares <jdop2000@gmail.com>
2020-03-19 19:53:38 +0000

    function that prints a singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 
 	if (h->str == NULL)
 	{
-		printf("[%d] (nil)\n", h->len);
+		printf("[0] (nil)\n");
 		return (print_list(h->next) + 1);
 	}
 	printf("[%d] %s\n", h->len, h->str);
