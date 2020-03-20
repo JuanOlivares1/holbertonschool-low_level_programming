45d9cd1034e58b8b84f0edd13cf3af1dff7b4b51
Juan Olivares <jdop2000@gmail.com>
2020-03-20 02:25:46 +0000

    I win...

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * start - calculate the lenght of a linked list
+ *
+ * Return: void
+ */
+void start(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
