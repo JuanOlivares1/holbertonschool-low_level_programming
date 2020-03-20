3fb9077aea5a8e2f603c09301bd51ceb7d71501c
Juan Olivares <jdop2000@gmail.com>
2020-03-20 02:10:46 +0000

    refactoring function

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,12 +8,10 @@
  */
 void free_list(list_t *head)
 {
-	list_t *temp = head;
-
-	if (temp == NULL)
+	if (head == NULL)
 		return;
 
 	free_list(head->next);
-	free(temp->str);
-	free(temp);
+	free(head->str);
+	free(head);
 }
