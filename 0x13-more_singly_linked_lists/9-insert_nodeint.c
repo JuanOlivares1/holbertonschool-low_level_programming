dc9c57fc60b81b143f6be532350baa19ce3238c5
Juan Olivares <jdop2000@gmail.com>
2020-03-24 21:39:15 +0000

    creating a function that adds a node on a index position from list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (idx == 0)
 		return (add_nodeint(&(*head), n));
 
+	if (*head == NULL)
+		return (NULL);
+
 	return (insert_nodeint_at_index(&(head2->next), idx - 1, n));
 }
 
