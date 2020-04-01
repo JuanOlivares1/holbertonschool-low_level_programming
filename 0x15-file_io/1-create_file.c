88c29cf7189a1cca84a6c8d500ccd6f9d2bee5e6
Juan Olivares <jdop2000@gmail.com>
2020-04-01 03:10:46 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,6 @@ int create_file(const char *filename, char *text_content)
 	ssize_t wr;
 	int fd, i = 0;
 
-	if (filename == NULL)
-		return (-1);
-
 	for (i = 0; text_content && text_content[i]; i++)
 		;
 
