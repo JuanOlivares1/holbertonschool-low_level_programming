c8e455ac850b6cb3ccf705bab8104b08ca774b76
Juan Olivares <jdop2000@gmail.com>
2020-05-02 22:54:04 +0000

    c function - adds node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-#include "5-get_dnodeint.c"
 
 /**
  * insert_dnodeint_at_index - adds nodes at index in a doubly linked list
@@ -26,4 +25,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	temp->prev = new;
 	new->next = temp;
 	return (new);
+}
+
+/**
+ * get_dnodeint_at_index - gets node from a doubly linked list at index
+ * @head: lisit's head
+ * @index: index
+ *
+ * Return: node at index
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head == NULL)
+		return (NULL);
+
+	while (index != 0)
+	{
+		head = head->next;
+		index--;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
 }
