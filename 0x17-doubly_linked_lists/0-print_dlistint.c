7239ca1691acdf4448eea94a4bc3ead559188ddb
Juan Olivares <jdop2000@gmail.com>
2020-05-02 19:46:11 +0000

    c program - prints a doubly linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints a doubly linked list
+ * @h: lsit's head
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nnodes = 0;
+
+	if (h == NULL)
+		return (nnodes);
+
+	while (h != NULL)
+	{
+		nnodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return(nnodes);
+}
