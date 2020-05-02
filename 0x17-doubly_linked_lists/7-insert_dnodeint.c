6f89265e1d02a65d8337bdb1625364476bb15cf3
Juan Olivares <jdop2000@gmail.com>
2020-05-02 23:31:39 +0000

    c function - adds node at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,6 +16,8 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
+		if (new == null)
+			return (NULL);
 		return (new);
 	}
 
