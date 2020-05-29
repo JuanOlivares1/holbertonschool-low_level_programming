9310ebd945b2f1c39fb774cf827dfde6f71601e5
Juan Olivares <jdop2000@gmail.com>
2020-05-29 16:30:39 +0000

    refactoring function 'hash_table_create'

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-#include <string.h>
 /**
  * hash_table_create - creates hash table
  * @size: size of array
@@ -10,14 +9,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
 
-	ht = calloc(sizeof(hash_node_t), 1);
+	if (size < 1)
+		return(NULL);
+
+	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
-	ht->array = calloc(sizeof(hash_node_t *), size);
+	ht->size = size;
+	ht->array = calloc(size, sizeof(hash_node_t *));
 	if (ht->array == NULL)
-	{
-		free(ht);
 		return (NULL);
-	}
 	return (ht);
 }
