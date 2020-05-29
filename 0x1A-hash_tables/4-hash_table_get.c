b800574569efe039286863258682cd779e9b2a28
Juan Olivares <jdop2000@gmail.com>
2020-05-29 19:33:01 +0000

    defining 'hash_table_get' function

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,15 +12,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *head = NULL;
 
-	if (!key || strcmp(key, "") == 0)
+	if (ht == NULL || key == NULL)
 		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	head = ht->array[index];
-	if (head == NULL)
-		return (NULL);
-	while(strcmp(head->key, key) != 0 && head->next != NULL)
+
+	while (head)
+	{
+		if (strcmp(head->key, key) == 0)
+			return (head->value);
 		head = head->next;
-	if (strcmp(head->key, key) == 0)
-		return (head->value);
+	}
 	return (NULL);
 }
